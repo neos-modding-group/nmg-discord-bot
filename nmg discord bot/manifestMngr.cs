7087ba097e01cebd3278aef091ef9445c29c5490
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-07-31 04:40:30 -0700

    add spaces

---
+++ nmg discord bot/manifestMngr.cs
@@ -0,0 +1,6 @@
+﻿namespace nmgBot
+{
+    internal class manifestMngr
+    {
+    }
+}
---
+++ nmg discord bot/msgFileMngr.cs
@@ -0,0 +1,25 @@
+﻿using Discord.WebSocket;
+
+namespace nmgBot
+{
+    static internal class msgFileMngr
+    {
+        internal static async void SetUp()
+        {
+            BotMngr.client.MessageReceived += MsgHandler;
+        }
+
+        private static async Task MsgHandler(SocketMessage msg)
+        {
+            foreach (var att in msg.Attachments)
+            {
+                logWraper.Debug(att.Filename);
+                logWraper.Debug(att.Url);
+                logWraper.Debug(att.ProxyUrl);
+                logWraper.Debug(att.ContentType);
+                logWraper.Debug(att.Description);
+                logWraper.Debug(att.ToString());
+            }
+        }
+    }
+}
